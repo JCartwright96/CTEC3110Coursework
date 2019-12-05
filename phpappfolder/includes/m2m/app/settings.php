c148c550ec78cf640fb2422fb1fe2c7546a66bef
Alfie Loakes <alfieloakes@gmail.com>
2019-12-05 14:19:30 +0000

    Added readme.txt explaining message format

--- phpappfolder/includes/m2m/app/settings.php
+++ phpappfolder/includes/m2m/app/settings.php
@@ -7,6 +7,35 @@ define('APP_URL', $app_url);
 $wsdl = 'https://m2mconnect.ee.co.uk/orange-soap/services/MessageServiceByCountry?wsdl';
 define('WSDL', $wsdl);
 
+$settings = [
+    "settings" => [
+        'determineRouteBeforeAppMiddleware' => false,
+        'displayErrorDetails' => true,
+        'addContentLengthHeader' => false,
+        'mode' => 'development',
+        'debug' => true,
+        'view' => [
+            'template_path' => __DIR__ . '/templates/',
+            'twig' => [
+                'cache' => false,
+                'debug' => true,
+                'auto_reload' => true,
+            ],
+            ],
+    ],
+    'doctrine_settings' => [
+        'driver' => 'pdo_mysql',
+        'host' => 'localhost',
+        'dbname' => 'registered_users_db',
+        'port' => '3306',
+        'user' => 'root',
+        'password' => '',
+        'charset' => 'utf8mb4'
+    ],
+];
+
+return $settings;
+
 return [
     'settings' => [
         // Slim Settings
@@ -25,6 +54,15 @@ return [
         'logger' => [
             'name' => 'app',
             'path' => __DIR__ . '/../log/app.log',
+        ],
+        'doctrine_settings' => [
+            'driver' => 'pdo_mysql',
+            'host' => '127.0.0.1',
+            'dbname' => 'ctec3110',
+            'port' => '3306',
+            'user' => 'root',
+            'password' => '',
+            'charset' => 'utf8mb4'
         ],
     ],
 ];
