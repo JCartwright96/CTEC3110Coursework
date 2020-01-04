0c0ede187f5bb68de360d8dee6bd891c4b42c8a9
TomMcGreevy <45593803+TomMcGreevy@users.noreply.github.com>
2020-01-04 03:05:51 +0000

    added tags for current settings
    I wrote this before properly taking a look at how you were doing hompage.html.twig.
    Not sure which way you would prefer to go with it

--- phpappfolder/includes/m2m/app/routes/homepage.php
+++ phpappfolder/includes/m2m/app/routes/homepage.php
@@ -2,15 +2,29 @@
 
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
+use Doctrine\DBAL\DriverManager;
 
-$app->get('/', function(Request $request, Response $response)
+$app->get('/', function(Request $request, Response $response) use ($app)
 {
     $sid = session_id();
+    $current_settings = getCurrentSettingsDetails($app);
+    $current_settings = $current_settings[0];
 
+
+    $message_id = $current_settings['message_id'];
+    $phone_number = $current_settings['phone_number'];
+    $switch_01 = $current_settings['switch_01'];
+    $switch_02 = $current_settings['switch_02'];
+    $switch_03 = $current_settings['switch_03'];
+    $switch_04 = $current_settings['switch_04'];
+    $fan = $current_settings['fan'];
+    $heater = $current_settings['heater'];
+    $keypad = $current_settings['keypad'];
+//    $receivedtime = $current_settings['receivedtime'];
+    $timestamp = $current_settings['timestamp'];
     $messages_link = $this->router->pathFor('messages');
     $login_link = $this->router->pathFor('login');
     $register_link = $this->router->pathFor('registeruserform');
-
     return $this->view->render($response,
         'homepage.html.twig',
         [
@@ -19,6 +33,18 @@ $app->get('/', function(Request $request, Response $response)
             'messages_link' => $messages_link,
             'login_link' => $login_link,
             'register_link' => $register_link,
+            'currentsettings' => $current_settings,
+            'message.message_id' => $message_id,
+            'message.phone_number' => $phone_number,
+            'message.switch_01' => $switch_01,
+            'message.switch_02' => $switch_02,
+            'message.switch_03' => $switch_03,
+            'message.switch_04' => $switch_04,
+            'message.fan' => $fan,
+            'message.heater' => $heater,
+            'message.keypad' => $keypad,
+//            'message.receivedtime' => $receivedtime,
+            'message.timestamp' => $timestamp,
             'landing_page' => $_SERVER["SCRIPT_NAME"],
             'action' => 'storesessiondetails',
             'initial_input_box_value' => null,
@@ -31,3 +57,16 @@ $app->get('/', function(Request $request, Response $response)
             'sid' => $sid,
         ]);
 })->setName('homepage');
+
+
+function getCurrentSettingsDetails($app) {
+    $messages = [];
+
+    $database_connection_settings = $app->getContainer()->get('doctrine_settings');
+    $doctrine_queries = $app->getContainer()->get('doctrineSqlQueries');
+    $database_connection = DriverManager::getConnection($database_connection_settings);
+
+    $messages = $doctrine_queries::queryGetLatestMessageData($database_connection);
+
+    return $messages;
+}
